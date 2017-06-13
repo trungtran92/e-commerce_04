bbc05579b1c9c3550e915faa2dc958b0255e39ee
trungtran <trantrung92.ttt@gmail.com>
2017-06-13 16:05:27 +0700

    static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def show
+    if valid_page?
+      render template: "static_pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+
+  def valid_page?
+    File.exist? Pathname.new(Rails.root + "app/views/static_pages/#{params[:page]}.html.erb")
+  end
+end
