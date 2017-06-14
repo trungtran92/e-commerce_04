911fe1f0c9e6c8cb729f056fce887c737caee314
trungtran <trantrung92.ttt@gmail.com>
2017-06-14 08:55:00 +0700

    product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def require_login
+    return if logged_in?
+    flash[:danger] = t "please_log_in"
+    redirect_to login_path
+  end
 end
