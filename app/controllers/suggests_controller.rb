2ec2610cdaed97dbd48adef145fe70344a1d4068
trungtran92 <trungtran92.ttt@gmail.com>
2017-06-23 02:22:15 +0700

    suggest

---
+++ app/controllers/suggests_controller.rb
@@ -0,0 +1,25 @@
+class SuggestsController < ApplicationController
+  before_action :require_login
+
+  def new
+    @suggests = Suggest.new
+  end
+
+  def create
+    @suggest = Suggest.new suggest_params
+    @suggest.user =  current_user
+    if @suggest.save
+      flash[:success] = t "flash.success.create_suggest_cuccess"
+      redirect_to  root_path
+    else
+      flash.now[:danger] = t "flash.danger.create_suggest_fail"
+      render :new
+    end
+  end
+
+  private
+
+  def suggest_params
+    params.require(:suggest).permit :product_name, :product_description
+  end
+end
