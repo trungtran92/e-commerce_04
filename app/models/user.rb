6ae146bed6202788f99e795f2a4b084908de9cab
trungtran <trantrung92.ttt@gmail.com>
2017-06-29 12:56:41 +0700

    authfb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,32 @@
 class User < ApplicationRecord
+  ratyrate_rater
+
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :twitter]
   has_many :orders, dependent: :destroy
   has_many :suggests, dependent: :destroy
 
-  validates :name, presence: true, length: {maximum: Settings.name_user_maximum}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.email_maximum},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+    format: {with: VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+      uniqueness: {case_sensitive: false}
   validates :password, presence: true,
     length: {minimum: Settings.password_minimum}, allow_nil: true
 
-  has_secure_password
-  ratyrate_rater
+  def self.new_with_session params, session
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&
+        session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end
 end
